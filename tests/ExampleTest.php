6053bdb7524ff0798caa6f468f83998fdbab503b
umuttaymaz <taymazumut@gmail.com>
2018-03-01 11:48:53 +0300

    example test

---
+++ tests/ExampleTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: umuttaymaz
+ * Date: 1.03.2018
+ * Time: 11:47
+ */
+
+namespace UmutTaymaz\VerimorSMSAPI\tests;
+
+
+class ExampleTest extends \PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
